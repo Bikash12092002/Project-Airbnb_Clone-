6e67f517110083ca66a6688f7982b842fcd834c9
Bikash Kumar Barik <bikashkumarbarik2002@gmail.com>
2025-10-21 11:42:05 +0530

    Fix port for Render deployment

--- app.js
+++ app.js
@@ -101,6 +101,8 @@ app.use((err,req,res,next)=>{
   res.status(statusCode).render("error.ejs",{message})
   // res.status(statusCode).send(err);
 });
-app.listen(8080,()=>{
-console.log('Server is running on port 8080');
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
